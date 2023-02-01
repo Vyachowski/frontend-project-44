efae57fb506edb4ff9fa6ea3ecac3f74bc191484
Vyacheslav Andreev <97975418+Vyachowski@users.noreply.github.com>
2023-02-01 20:52:08 +0200

    Добавляет исправление логики игры

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,19 +4,22 @@ import readlineSync from 'readline-sync';
 import welcome from '../src/cli.js';
 //MAIN VARIABLES
 let answer;
+let wrongAnswer = 0;
 let score = 0;
-const numbers = threeNumbersWithEveness();
+const hiddenNumbers = threeNumbersWithEveness();
 const name = welcome();
 //MAIN LOGIC
-for(let i = 0; i < 3; i += 1) {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ' + numbers[i][0]);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+for(let i = 0; i < 3 && wrongAnswer < 1; i += 1) {
+  console.log('Question: ' + hiddenNumbers[i][0]);
   answer = readlineSync.question('Your answer: ').toLowerCase();
-  if (answer === numbers[i][1]) {
+  if (answer === hiddenNumbers[i][1]) {
     console.log('Correct!');
     score += 1;
+  } else {
+  console.log('Answer \'' + answer + '\' is wrong answer ;(. Correct answer was \'' + hiddenNumbers[i][1] + '\'. Let\'s try again, ' + name + '!');
+  wrongAnswer += 1;
   }
-  console.log('Answer \'' + answer + '\' is wrong answer ;(. Correct answer was \'' + numbers[i][1] + '\'. Let\'s try again, ' + name + '!');
-  break
 }
 score === 3 ? console.log('Congratulations, ' + name + '!') : false;
 //FUNCTIONS
