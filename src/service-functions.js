c0360d1e6e207b098357877134cd0c13300612fd
Vyacheslav Andreev <97975418+Vyachowski@users.noreply.github.com>
2023-03-02 20:55:06 +0200

    Add small fix

--- src/service-functions.js
+++ src/service-functions.js
@@ -1,11 +1,13 @@
 const getBlankResults = () => [[[], []], [[], []], [[], []]];
 const getRandomInt = (max, min = 1) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 const getCommonDiv = (a, b) => {
   if (!b) {
     return a;
   }
   return getCommonDiv(b, a % b);
 };
+
 const isPrime = (num) => {
   if (num % 1 || num < 2) {
     return false;
