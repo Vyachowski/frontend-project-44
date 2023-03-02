1e265ad5ef32a6318947d135989df17bd46db825
Vyacheslav Andreev <97975418+Vyachowski@users.noreply.github.com>
2023-03-02 12:35:51 +0200

    Bug fixes

--- src/answers.js
+++ src/answers.js
@@ -1,10 +1,10 @@
 export default function numbersWithEveness() {
   const result = [
-    [[],[]],
-    [[],[]],
-    [[],[]]
+    [[], []],
+    [[], []],
+    [[], []],
   ];
-  for(let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     result[i][0] = Math.round(Math.random() * 100);
     result[i][1] = result[i][0] % 2 === 0 ? 'yes' : 'no';
   }
@@ -13,28 +13,26 @@ export default function numbersWithEveness() {
 
 export function expressionsWithEquality() {
   const result = [
-    [[],[]],
-    [[],[]],
-    [[],[]]
+    [[], []],
+    [[], []],
+    [[], []],
   ];
   for (let i = 0; i < 3; i += 1) {
     const randomNumber1 = Math.round(Math.random() * 100);
-    const randomNumber2 = + Math.round(Math.random() * 100);
-    const mathSigns = ['+', '-', '*'];
+    const randomNumber2 = Math.round(Math.random() * 100);
     const randomSign = Math.floor(Math.random() * 3 + 1);
     switch (randomSign) {
       case 1:
-        result[i][0] = randomNumber1 + ' + ' + randomNumber2;
+        result[i][0] = `${randomNumber1} + ${randomNumber2}`;
         result[i][1] = String(randomNumber1 + randomNumber2);
         break;
       case 2:
-        result[i][0] = randomNumber1 + ' - ' + randomNumber2;
+        result[i][0] = `${randomNumber1} - ${randomNumber2}`;
         result[i][1] = String(randomNumber1 - randomNumber2);
         break;
-      case 3:
-        result[i][0] = randomNumber1 + ' * ' + randomNumber2;
+      default:
+        result[i][0] = `${randomNumber1} * ${randomNumber2}`;
         result[i][1] = String(randomNumber1 * randomNumber2);
-        break;
     }
   }
   return result;
--- src/gameengine.js
+++ src/gameengine.js
@@ -1,24 +1,24 @@
 import readlineSync from 'readline-sync';
-import welcome from '../src/cli.js';
+import welcome from './cli.js';
 
 export default function gameEngine(gameRule, answers) {
-  //Variables
+  //  Variables
   let userAnswer = '';
   let score = 0;
   let defeat = false;
   const name = welcome();
-  //Main Logic
+  //  Main Logic
   console.log(gameRule);
-  for(let i = 0; i < 3 && defeat === false; i += 1) {
-    console.log('Question: ' + answers[i][0]);
+  for (let i = 0; i < 3 && defeat === false; i += 1) {
+    console.log(`Question: ${answers[i][0]}`);
     userAnswer = readlineSync.question('Your answer: ').toLowerCase();
     if (userAnswer === answers[i][1]) {
       console.log('Correct!');
       score += 1;
     } else {
-    console.log(`Answer ${userAnswer} is wrong answer ;(. Correct answer was ${answers[i][1]}. Let's try again, ${name}!`);
-    defeat = true;
+      defeat = true;
+      return console.log(`Answer ${userAnswer} is wrong answer ;(. Correct answer was ${answers[i][1]}. Let's try again, ${name}!`);
     }
   }
-  score === 3 ? console.log('Congratulations, ' + name + '!') : false;
+  return score === 3 ? console.log(`Congratulations, ${name}!`) : false;
 }
