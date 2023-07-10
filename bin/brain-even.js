ab0b0526b37e5e83e0e1823b452ab82e72bf79b4
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-10 20:55:58 +0300

    Ошибка пути

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameEngine from '../src/game-engine.js';
-import brainEven from '../src/games/brainEven.js';
+import brainEven from '../src/games/brainEvenly.js';
 
 const question = 'Answer "yes" if the number is even, otherwise answer "no".';
 const roundRules = [brainEven(), brainEven(), brainEven()];
