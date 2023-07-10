c2f323c08a557f8de9c75e011e06e0392136e8e2
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-10 20:38:51 +0300

    refactor: меняет имена файлов

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameEngine from '../src/game-engine.js';
-import brainCalc from '../src/games/braincalc.js';
+import brainCalc from '../src/games/brainСalc.js';
 
 const question = 'What is the result of the expression?';
 const roundRules = [brainCalc(), brainCalc(), brainCalc()];
