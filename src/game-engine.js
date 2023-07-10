6f31ad004cc31ebd237df2440af87e71bf64e63e
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-10 14:10:57 +0300

    refactor: Добавляет пустые строки для удобства чтения

--- src/game-engine.js
+++ src/game-engine.js
@@ -3,7 +3,9 @@ import { getUserName, readlineSync } from './tools.js';
 const gameEngine = (question, roundRules) => {
   const roundsToWin = 3;
   const userName = getUserName();
+
   console.log(question);
+
   for (let currentRound = 0; currentRound < roundsToWin; currentRound += 1) {
     const [task, result] = roundRules[currentRound];
     console.log(`Question: ${task}`);
@@ -11,12 +13,11 @@ const gameEngine = (question, roundRules) => {
     const rightAnswer = result;
     if (userAnswer !== rightAnswer) {
       console.log(`Answer ${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}. Let's try again, ${userName}!`);
-      return false;
+      return;
     }
     console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
-  return true;
 };
 
 export default gameEngine;
