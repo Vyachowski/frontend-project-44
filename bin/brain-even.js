5211d216a197e090214041dac1a67f4396f98475
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-10 20:58:13 +0300

    Обратно меняет имя

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameEngine from '../src/game-engine.js';
-import brainEven from '../src/games/brainEvenly.js';
+import brainEven from '../src/games/brainEven.js';
 
 const question = 'Answer "yes" if the number is even, otherwise answer "no".';
 const roundRules = [brainEven(), brainEven(), brainEven()];
