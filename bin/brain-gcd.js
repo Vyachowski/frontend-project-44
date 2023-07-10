a6e5252e3f0de8d0e6a3bdf80af6e7e631d4adb3
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-07-10 21:00:04 +0300

    Меняет пути файлов для теста

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import gameEngine from '../src/game-engine.js';
-import brainGcd from '../src/games/brainGcd.js';
+import brainGcd from '../src/games/brainGcd111.js';
 
 const question = 'Find the greatest common divisor of given numbers.';
 const roundRules = [brainGcd(), brainGcd(), brainGcd()];
